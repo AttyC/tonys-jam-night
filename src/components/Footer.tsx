9f85a89bdc87f7392a363ab3bb2860d12ac35f82
Atty Cronin <antoniajcronin@gmail.com>
2023-12-21 14:43:29 +0000

    Upadte footer text

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,7 +4,7 @@ import Nav from './Nav';
 export const Footer: React.FC = () => {
     return (
         <footer className="bg-orange-700 py-8 text-white font-semibold flex flex-col items-center">
-            <p className="pb-4">Created by Team B - Tech Returners 2023</p>
+            <p className="pb-4">Created by Atty C 2023</p>
             <Nav />
         </footer>
     );
