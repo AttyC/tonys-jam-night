dc5a07a26ab20c26b3798ecad377fc9fc5e6b54d
Atty Cronin <antoniajcronin@gmail.com>
2023-12-21 22:06:13 +0000

    Remove references to Zebra

--- src/images/Images.tsx
+++ src/images/Images.tsx
@@ -1,8 +1,4 @@
 const IMAGES = {
-    // logoDarkTransparent: new URL(
-    //     './Project_Zebra_transparent_dark.png',
-    //     import.meta.url,
-    // ).href,
     logoDarkTransparent: new URL('./buglehorn.webp', import.meta.url).href,
     logoLightTransparent: new URL(
         './tonys_function_junction-logo-white.png',
