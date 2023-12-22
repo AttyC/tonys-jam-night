0ba90f7e197e234d31c2ca3583c979f30596112d
Atty Cronin <antoniajcronin@gmail.com>
2023-12-22 15:37:24 +0000

    Add images

--- src/images/Images.tsx
+++ src/images/Images.tsx
@@ -8,7 +8,7 @@ const IMAGES = {
         './male-acoustic-guitar-player-in-pale-light.png',
         import.meta.url,
     ).href,
-    mic: new URL('./simple-microphone.webp', import.meta.url).href,
+    whatsApp: new URL('./WhatsAppButtonWhiteSmall.png', import.meta.url).href,
 };
 
 // eslint-disable-next-line react-refresh/only-export-components
