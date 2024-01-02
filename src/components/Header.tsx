cfca0a837c6efe8b8f052f02c9e86d52d54167a6
Atty Cronin <antoniajcronin@gmail.com>
2024-01-02 16:35:46 +0000

    Sall CSS tweak for header nv

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -3,8 +3,8 @@ import Nav from './Nav';
 
 export const Header: React.FC = () => {
     return (
-        <header className="top-0 left-0 w-full rounded-t text-white py-4 flex items-center justify-between border-b-2 border-double border-b-orange-400">
-            <div className="logo pl-6 text-2xl md:text-4xl font-black tracking-widest">
+        <header className="top-0 left-0 w-full rounded-t text-white py-4 flex items-center justify-around md:justify-between border-b-2 border-double border-b-orange-400">
+            <div className="logo pl-6 text-lg md:text-4xl font-black tracking-widest">
                 <a
                     href="#top"
                     className=" text-white font-semibold flex align-middle hover:no-underline hover:border-orange-400 border-2 rounded-full"
