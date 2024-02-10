4dd731e93f8b11bf9aaf4e8a4fab49d33be09d37
Atty Cronin <antoniajcronin@gmail.com>
2024-02-10 16:39:08 +0000

    Reduce padding and boldness

--- src/components/EventType.tsx
+++ src/components/EventType.tsx
@@ -9,21 +9,21 @@ interface EventTypeProps {
 
 const ShowcaseText = () => (
     <>
-        <p className="text-md md:text-lg px-4 md:px-8 pb-2 font-semibold text-center">
+        <p className="text-md md:text-lg py-2 font-medium text-center">
             Please contact me if you wish to do a few songs with your band, or
             use the services of the house band!
         </p>{' '}
-        <ul className="px-8">
-            <li className="text-md md:text-lg px-4 py-2">
+        <ul className="">
+            <li className="text-md md:text-lg  py-2">
                 <span className="font-semibold">Guitarists and bassists</span>,
                 just bring your guitars.
             </li>
-            <li className="text-md md:text-lg px-4 py-2">
+            <li className="text-md md:text-lg py-2">
                 <span className="font-semibold">Drummers</span> need only bring
                 sticks - full drum set supplied, including all breakables:
                 snare, cymbals etc.
             </li>
-            <li className="text-md md:text-lg px-4 py-2">
+            <li className="text-md md:text-lg  py-2">
                 <span className="font-semibold">Singers</span>, microphones will
                 be supplied.
             </li>
@@ -32,25 +32,18 @@ const ShowcaseText = () => (
 );
 const JamText = () => (
     <>
-        <p className="text-md md:text-lg px-2 pb-2 font-semibold text-center">
+        <p className="text-md md:text-lg py-2 font-medium text-center">
             Here is a chance to jam and network with other musicians and
             singers. Full back line supplied.
         </p>{' '}
-        <h3 className="text-center font-medium uppercase py-3">
-            Back line equipment
-        </h3>
-        <ul className="list-disc mx-2 md:flex md:flex-col md:justify-center md:items-center">
-            <li className="text-md md:text-lg px-4 py-2">
-                Guitar and bass amps
+        <ul className="mx-2">
+            <li className=" px-4 py-2">- Guitar and bass amps</li>
+            <li className=" px-4 py-2">
+                - Full Pearl drum kit with cymbals ( Zildian and Sabian )
             </li>
-            <li className="text-md md:text-lg px-4 py-2">
-                Full Pearl drum kit with cymbals ( Zildian and Sabian )
-            </li>
-            <li className="text-md md:text-lg px-4 py-2">
-                Korg Krome piano synth with pedal
-            </li>
-            <li className="text-md md:text-lg px-4 py-2">
-                Full mackie PA with Foldback and vocal mics
+            <li className="px-4 py-2">- Korg Krome piano synth with pedal</li>
+            <li className=" px-4 py-2">
+                - Full mackie PA with Foldback and vocal mics
             </li>
         </ul>
     </>
@@ -62,7 +55,7 @@ const EventType: React.FC<EventTypeProps> = (props) => {
     return (
         <div className="col-md-6">
             <div
-                className={`h-100 p-5 text-${style.text} bg-${style.bg} rounded-3`}
+                className={`h-100 p-4 text-${style.text} bg-${style.bg} rounded-3`}
             >
                 <Title id={id} title={title}></Title>
                 <Text />
