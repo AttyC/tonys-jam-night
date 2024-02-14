7087bf60d43f2039e11d68dd1fc4cf27178fd738
Atty Cronin <antoniajcronin@gmail.com>
2024-02-14 14:58:31 +0000

    Update next gig date

--- src/components/Stamp.tsx
+++ src/components/Stamp.tsx
@@ -1,7 +1,6 @@
 const Stamp = () => (
     <div className="stamp bg-pink-400 text-black text-center text-sm md:text-md lg:text-xl py-2 mb-4 font-semibold">
-        <p>Next session : Sunday 11 Feb from 5pm</p>
-        {/* <img src="../../public/img/stamp.png" /> */}
+        <p>Next session : Sunday 24 March from 5pm</p>
     </div>
 );
 
